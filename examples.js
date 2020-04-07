d85a02293768a1f86eb8ce0f1e71897a87b911c6
llsanketll <55257633+llsanketll@users.noreply.github.com>
2020-04-08 02:25:11 +0545

    Adding the example to the server

---
+++ examples.js
@@ -0,0 +1,11 @@
+// Making a function
+
+    function print(a){
+        console.log(a);
+    }
+
+    // Another way to define funciton
+    var multiply = function(a,b){
+        return a*b;
+    } 
+    var p = multiply(5,6);//This returns 5x6
